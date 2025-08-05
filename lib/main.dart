df0def08671cb77ac4909a76360d8a052313b77a
kareemezzat <kareemezzat900@gmail.com>
2025-08-05 19:19:42 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckoutPaymentApp());
+}
+
+class CheckoutPaymentApp extends StatelessWidget {
+  const CheckoutPaymentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', home: Scaffold());
+  }
+}
